52f232166d8af2c16fd4e1957d651c6a21b6266b
Sashkata <16367921+Sashkata@users.noreply.github.com>
2022-09-08 21:03:38 -0500

    Add function passed from App to SearchBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchBar from './SearchBar';
 
-import '../styles/app.css'
+import '../styles/app.css';
 
 const App = () => {
+    const onSearchSubmit = term => {
+        console.log(term);
+    };
+
     return (
         <div className='hdr ui container '>
-            <SearchBar />
+            <SearchBar onSubmit={onSearchSubmit} />
         </div>
     );
-}
+};
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,24 @@
 import React, { useState } from 'react';
 
-const SearchBar = () => {
-    const [search, setSearch] = useState('');
+const SearchBar = ({ onSubmit }) => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const onFormSubmit = event => {
+        event.preventDefault();
+        onSubmit(searchTerm);
+    };
 
     return (
         <div className='ui segment'>
-            <form className='ui form'>
+            <form className='ui form' onSubmit={onFormSubmit}>
                 <div className='field'>
                     <label htmlFor='search'>Image Search</label>
                     <input
                         id='search'
                         type='text'
                         placeholder='Search Term Here'
-                        onChange={e => setSearch(e.target.value)}
-                        value={search}
+                        onChange={e => setSearchTerm(e.target.value)}
+                        value={searchTerm}
                     />
                 </div>
             </form>
