2c5d5210e05f6728b43dbae321c3e24a863e351b
Sashkata <16367921+Sashkata@users.noreply.github.com>
2022-09-08 21:30:38 -0500

    add state to hold images fetched from unsplash api

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
 
 import '../styles/app.css';
 
 const App = () => {
-    const onSearchSubmit = term => {
-        axios.get('https://api.unsplash.com/search/photos', {
-            params: { query: term },
-            headers: {
-                Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_API_KEY}`,
-            },
-        });
+    const [images, setImages] = useState([]);
+
+    const onSearchSubmit = async term => {
+        const response = await axios.get(
+            'https://api.unsplash.com/search/photos',
+            {
+                params: { query: term },
+                headers: {
+                    Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_API_KEY}`,
+                },
+            }
+        );
+
+        setImages(response.data.results);
     };
 
     return (
         <div className='hdr ui container '>
             <SearchBar onSubmit={onSearchSubmit} />
+            Found: {images.length} images
         </div>
     );
 };
